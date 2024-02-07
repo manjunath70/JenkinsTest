fdb4d6644bb03e3edfd906c9408f6fee0d99b4be
manjunath70 <manjunath7068@gmail.com>
2024-02-07 12:56:02 +0530

    Logger has been added

--- src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinsTestApplication.java
@@ -1,5 +1,7 @@
 package com.example.jenkinstest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class JenkinsTestApplication {
+//    Logger logger = LoggerFactory.getLogger(JenkinsTestApplication.class);
+    private static final Logger logger = LogManager.getLogger(JenkinsTestApplication.class);
 
     @GetMapping("/getMessage")
     public String getMessage() {
@@ -16,6 +20,9 @@ public class JenkinsTestApplication {
 
     public static void main(String[] args) {
         System.out.printf("Starting ... the server ");
+        logger.info("Hello world");
+        logger.info("we are in logger info mode");
+
         SpringApplication.run(JenkinsTestApplication.class, args);
         System.out.printf("Startingdfsasfas ... the server ");
 
